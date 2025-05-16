588b87ca03858afa37c6393b9ab062365e1343e1
Vinicius da Conceição Teixeira <viniciuscteixeira.rj@gmail.com>
2025-05-16 18:10:56 -0300

    Update Doc

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Cria o servidor HTTP e o integra com o socket.io
 const server = http.createServer(app);
